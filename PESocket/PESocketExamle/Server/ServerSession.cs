983cbbda1e570e979b25ae69fc6fd61b9fdf9476
DKS-20160923UBY\Administrator <359087005@qq.com>
2019-02-15 15:48:57 +0800

    网络开局天秀

---
+++ PESocket/PESocketExamle/Server/ServerSession.cs
@@ -0,0 +1,24 @@
+﻿using PENet;
+using Protocal;
+
+namespace Server
+{
+    public class ServerSession : PENet.PESession<NetMsg>
+    {
+
+        protected override void OnConnected()
+        {
+            PETool.LogMsg("CLient Connect");
+        }
+
+        protected override void OnDisConnected()
+        {
+            PETool.LogMsg("CLient DisConnect");
+        }
+
+        protected override void OnReciveMsg(NetMsg msg)
+        {
+            PETool.LogMsg("CLient ReciveMsg");
+        }
+    }
+}
---
+++ PESocket/PESocketExamle/Server/ServerStart.cs
@@ -0,0 +1,20 @@
+﻿using PENet;
+using Protocal;
+
+namespace Server
+{
+    public class ServerStart
+    {
+        static void Main(string[] arg)
+        {
+            PESocket<ServerSession, NetMsg> server = new PESocket<ServerSession, NetMsg>();
+
+            server.StartAsServer(IPCfg.srvIP,IPCfg.srvPort);
+            while (true)
+            {
+
+            }
+        }
+       
+    }
+}
